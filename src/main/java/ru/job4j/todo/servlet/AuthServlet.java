53e5abbd8294df51a0131985ab89e8051a27c26d
Илья Шокин <mallet322@gmail.com>
2022-03-09 08:45:14 +0300

    Refactoring

--- src/main/java/ru/job4j/todo/servlet/AuthServlet.java
+++ src/main/java/ru/job4j/todo/servlet/AuthServlet.java
@@ -30,7 +30,7 @@ public class AuthServlet extends HttpServlet {
         if (user != null && user.getPassword().equals(password)) {
             HttpSession sc = req.getSession();
             sc.setAttribute("user", user);
-            resp.sendRedirect(req.getContextPath() + "/index.jsp");
+            resp.sendRedirect(req.getContextPath() + "/index.do");
         } else {
             req.setAttribute("error", "Не верный email или пароль");
             req.getRequestDispatcher("login.jsp").forward(req, resp);
